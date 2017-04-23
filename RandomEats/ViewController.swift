4adda6ce63260b54a0634652ced34b0987129287
brandonwnguyen <brandonwnguyen@gmail.com>
2017-04-22 18:34:50 -0700

    got the map location to work

--- RandomEats/ViewController.swift
+++ RandomEats/ViewController.swift
@@ -11,11 +11,21 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //set initial location to SJSU
+        let initialLocation = CLLocation(latitude: 37.3352, longitude: -121.8811)
+        centerMapOnLocation(location: initialLocation)
+        
     }
-
 }
 
