5fcace3d3246ee8b0164d5e81957c8c56a42a534
Clarencewu94 <Clarencewu94@yahoo.com>
2017-04-22 18:16:54 -0700

    New Main Screen

---
+++ HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  RandomEats
+//
+//  Created by Student on 4/22/17.
+//  Copyright © 2017 Brandon Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+   
+    
+    @IBOutlet weak var Output: UILabel!
+    @IBOutlet weak var Input: UITextField!
+    
+    @IBAction func Randomize(_ sender: Any) {
+        
+          Output.text = "Hello, " + (Input.text)!
+    }
+    
+    
+    
+    
+    
+    
+}
--- RandomEats/ViewController.swift
+++ RandomEats/ViewController.swift
@@ -11,11 +11,11 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
     @IBOutlet weak var mapView: MKMapView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    
+    
 }
 
