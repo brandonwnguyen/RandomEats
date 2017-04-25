d185020f5a1983020ab77ffd8c312cf23793f25a
HungryByte <huntunzaw01@gmail.com>
2017-04-25 14:59:32 -0700

    Sign in and Sign up viewcontroller added

---
+++ RandomEats/TextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TextViewController.swift
+//  RandomEats
+//
+//  Created by Hun Tun Zaw on 4/25/17.
+//  Copyright © 2017 Brandon Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
